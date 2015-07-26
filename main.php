e64f8508f372cbe3ddc5833dd7a97bcaaacc0340
Huksley <huksley@sdot.ru>
2015-07-26 22:10:13 +0300

    Added ability to modify content type.

--- main.php
+++ main.php
@@ -2,21 +2,29 @@
 $text = plugin_load("renderer", "text");
 // Try to render with text plugin first
 if (!@$_REQUEST["do"] && $text != null) {
-	header("Content-Type: text/plain");
 	$id = $_SERVER["QUERY_STRING"];
 	$id = str_replace("/", ":", $id);
 	$id = str_replace("id=", "", $id);
 	$file = wikiFN($id);
-	$str = p_cached_output($file,'text');	
+	$str = p_cached_output($file,'text');
+	$contentType = "text/plain";
+	if (isset($GLOBALS["contentType"])) {
+		$contentType = $GLOBALS["contentType"];
+	}
+	if (!headers_sent()) header("Content-Type: $contentType");
 	echo $str;
 } else
 if (!@$_REQUEST["do"]) {
-	header("Content-Type: text/plain");
 	ob_start();
 	tpl_content(false);
 	$str = ob_get_contents();
 	ob_end_clean();
 	$str = trim(strip_tags($str));
+	$contentType = "text/plain";
+	if (isset($GLOBALS["contentType"])) {
+		$contentType = $GLOBALS["contentType"];
+	}
+	if (!headers_sent()) header("Content-Type: $contentType");
 	echo $str;
 } else {
 	tpl_content(false);
